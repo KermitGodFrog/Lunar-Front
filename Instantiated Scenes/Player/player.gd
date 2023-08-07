a6c0b169ff09a3779d09c7c8569758d1a3d1e041
KermitGodFrog <kaden@Kadens-MacBook-Pro.local>
2023-08-07 14:41:44 +0800

    Very small changes in spacing in Player script.

--- Instantiated Scenes/Player/player.gd
+++ Instantiated Scenes/Player/player.gd
@@ -28,7 +28,6 @@ signal commence_firing
 func _ready():
 	health.reset()
 	health.health_changed.connect(_on_health_changed)
-	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	game_data.player = self
 	pass
 
@@ -73,6 +72,8 @@ func movement(delta):
 	if Input.is_action_just_pressed("fa_toggle"):
 		is_fa_toggle = !is_fa_toggle
 	
+	#ACCELERATION
+	
 	var accelerate_dir = Input.get_axis("accelerate_backward", "accelerate_forward")
 	if accelerate_dir:
 		velocity += global_transform.basis.z * accelerate_dir * ACCELERATION_FORWARD
@@ -87,9 +88,7 @@ func movement(delta):
 		velocity += global_transform.basis.y * move_y_dir * ACCELERATION_MOVEMENT
 		is_accelerating = true
 	
-	#PITCH AND YAW ARE OPPOSITE
-	
-	
+	#ROTATION
 	
 	var pitch_axis = Input.get_axis("pitch_up", "pitch_down")
 	if pitch_axis:
@@ -99,7 +98,6 @@ func movement(delta):
 		if is_fa_toggle == true:
 			PITCH_TIME = lerp(PITCH_TIME, 0.0, ROTATION_INTERPOLATION)
 	
-	
 	var yaw_axis = Input.get_axis("yaw_right", "yaw_left")
 	if yaw_axis:
 		YAW_TIME = lerp(YAW_TIME, yaw_axis * YAW_SPEED, ROTATION_INTERPOLATION)
@@ -107,7 +105,6 @@ func movement(delta):
 	else:
 		if is_fa_toggle == true:
 			YAW_TIME = lerp(YAW_TIME, 0.0, ROTATION_INTERPOLATION)
-	
 	
 	var roll_axis = Input.get_axis("roll_left", "roll_right")
 	if roll_axis:
@@ -138,59 +135,9 @@ func weapons():
 	
 	if Input.is_action_pressed("commence_firing"):
 		emit_signal("commence_firing", selected_fire_group)
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	#var point_at_mouse_nodes = get_tree().get_nodes_in_group("point_at_mouse")
-	#for node in point_at_mouse_nodes:
-		#node.look_at(get_mouse_to_3d()[0], Vector3(0, 0, -1))
-	
-	#var new_point_at_mouse_nodes = get_tree().get_nodes_in_group("new_point_at_mouse")
-	#for node in new_point_at_mouse_nodes:
-		#var a = Vector2(get_mouse_to_3d()[0].x, get_mouse_to_3d()[0].z)
-		#var b = Vector2(node.get_child(2).global_transform.origin.x, node.get_child(2).global_transform.origin.z)
-		#var diff = Vector2(a.x - b.x, a.y - b.y)
-		#node.get_child(2).transform.basis = Basis()
-		#node.get_child(2).rotate_object_local(Vector3(0, 1, 0), -atan2(diff.y, diff.x))
-		#does not acocunt for ship rotation
-		#maybe work if in nodes own script? Or by adding ships rotation to it each time?
-	
-	
-	#var point_at_closest_enemy_nodes = get_tree().get_nodes_in_group("point_at_closest_enemy")
-	#for node in point_at_closest_enemy_nodes:
-		#var closest_enemy = game_data.get_closest_body(get_tree().get_nodes_in_group("enemy"), node.global_position)
-		#if closest_enemy != null:
-			#node.look_at(closest_enemy.transform.origin + closest_enemy.velocity * ((closest_enemy.velocity - velocity).length() / (velocity + -node.basis.z * BULLET_SPEED).length()))
-			#node.look_at(closest_enemy.transform.origin + closest_enemy.velocity * BULLET_SPEED)
-			#node.look_at(closest_enemy.transform.origin + closest_enemy.velocity * (closest_enemy.velocity - velocity).length() / BULLET_SPEED)
-	
-	
-	#if Input.is_action_pressed("fire"):
-		#if $fire_cooldown.is_stopped() == true:
-			#for node in point_at_closest_enemy_nodes:
-				#if game_data.manual_fire_check(self, node, node.get_child(1)) == true:
-					#game_data.spawn_pd_round(self, BULLET_SPEED, 1, node.get_child(0).global_transform)
-			#$fire_cooldown.start()
 	pass
 
-
+#UTILITY
 
 func get_mouse_to_3d():
 	var space_state = get_world_3d().direct_space_state
@@ -199,7 +146,6 @@ func get_mouse_to_3d():
 	var ray_end = ray_origin + get_viewport().get_camera_3d().project_ray_normal(mouse_position) * 1000
 	var intersection = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(ray_origin, ray_end, 1))
 	return [ray_end, intersection]
-
 
 func _on_health_changed(current_health):
 	if current_health == 0:
