fd1274e55ddc6024798865d25197666e1379282a
KermitGodFrog <kaden@Kadens-MacBook-Pro.local>
2023-08-29 20:08:14 +0800

    Saving, loading functionality and leaderboard API connection.

---
+++ Data/global_data.gd
@@ -0,0 +1,47 @@
+extends Node
+
+var player_name: String
+var scrapyard_best_time: float
+var asteroid_refinery_best_time: float
+
+func _ready():
+	load_player_data()
+	
+	SilentWolf.configure({
+		"api_key": "t17Djp2d1z8Ji8tZGVYNH63HNmxrwsAS5C6RT8tv",
+		"game_id": "lunar-front",
+		"log_level": 2,
+	})
+	
+	pass
+
+func _notification(what):
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		save_player_data()
+	pass
+
+func save_player_data():
+	var file = FileAccess.open("user://saved_best_times.save", FileAccess.WRITE)
+	file.store_var(player_name)
+	file.store_var(scrapyard_best_time)
+	file.store_var(asteroid_refinery_best_time)
+	file.close()
+	pass
+
+func load_player_data():
+	if FileAccess.file_exists("user://saved_best_times.save"):
+		var file = FileAccess.open("user://saved_best_times.save", FileAccess.READ)
+		player_name = file.get_var(true)
+		scrapyard_best_time = file.get_var(true)
+		asteroid_refinery_best_time = file.get_var(true)
+		file.close()
+	else:
+		player_name = ""
+		scrapyard_best_time = 0
+		asteroid_refinery_best_time = 0
+	pass
+
+func reset_player_data():
+	var file = FileAccess.open("user://saved_best_times.save", FileAccess.WRITE)
+	file.close()
+	pass
