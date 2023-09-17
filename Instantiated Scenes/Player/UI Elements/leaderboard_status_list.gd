ff7bfb6783108d130f0bb9551ada266292238eba
KermitGodFrog <kaden@Kadens-MacBook-Pro.local>
2023-09-17 18:22:33 +0800

    Texturing for all the junk rocks.

--- Instantiated Scenes/Player/UI Elements/leaderboard_status_list.gd
+++ Instantiated Scenes/Player/UI Elements/leaderboard_status_list.gd
@@ -15,12 +15,16 @@ func update():
 
 func render(scores):
 	clear()
-	add_item(str(global_data.player_name, ": "))
-	var add_player_score = add_item(str(snapped(global_data.get_best_time_current_map(), 0.001)))
-	set_item_custom_bg_color(add_player_score, Color.DARK_RED)
+	#add_item(str(global_data.player_name, ": "))
+	#var add_player_score = add_item(str(snapped(global_data.get_best_time_current_map(), 0.001)))
+	#set_item_custom_bg_color(add_player_score, Color.DARK_RED)
 	
 	for score in scores:
 		add_item(str(score.player_name, ": "))
 		var add_online_player_score = add_item(str(snapped(score.score, 0.001)))
-		set_item_custom_bg_color(add_online_player_score, Color.DARK_GREEN)
+		
+		if score.player_name == global_data.player_name:
+			set_item_custom_bg_color(add_online_player_score, Color.DARK_RED)
+		else:
+			set_item_custom_bg_color(add_online_player_score, Color.DARK_GREEN)
 	pass
