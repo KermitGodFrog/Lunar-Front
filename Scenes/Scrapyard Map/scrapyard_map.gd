653eda297cb59ff64094bb2321f772a7c6d94e35
KermitGodFrog <kaden@Kadens-MacBook-Pro.local>
2023-09-22 22:54:50 +0800

    Better cargo ship animation, map is pretty much done.

--- Scenes/Scrapyard Map/scrapyard_map.gd
+++ Scenes/Scrapyard Map/scrapyard_map.gd
@@ -4,6 +4,6 @@ func _ready():
 	game_data.base_node = self
 	map_data.map_identifier = "scrapyard"
 	map_data.checkpoints = [$checkpoint1, $checkpoint2, $checkpoint3, $checkpoint4, $checkpoint5, $checkpoint6, $checkpoint7, $checkpoint8, $checkpoint9]
-	map_data.boost_pickups = [$boost_pickup]
-	map_data.slingshot_pickups = [$slingshot_pickup]
+	map_data.boost_pickups = [$freighter/boost_pickup, $freighter/boost_pickup2]
+	map_data.slingshot_pickups = [$slingshot_pickup, $freighter/slingshot_pickup]
 	pass
