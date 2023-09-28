7b695b2e9a6cca0fc5a29b9f053295c26dea54c6
KermitGodFrog <kaden@Kadens-MacBook-Pro.local>
2023-09-28 19:17:41 +0800

    Complete fleet map layout.

--- Scenes/Fleet Map/fleet_map.gd
+++ Scenes/Fleet Map/fleet_map.gd
@@ -3,7 +3,7 @@ extends Node3D
 func _ready():
 	game_data.base_node = self
 	map_data.map_identifier = "fleet_map"
-	map_data.checkpoints = [$checkpoint, $checkpoint2, $checkpoint3, $checkpoint4, $checkpoint5, $checkpoint6, $checkpoint7]
-	map_data.boost_pickups = []
+	map_data.checkpoints = [$checkpoint, $checkpoint2, $checkpoint3, $checkpoint4, $checkpoint5, $checkpoint6, $checkpoint7, $checkpoint8, $checkpoint9, $checkpoint10, $checkpoint11, $checkpoint12, $checkpoint13]
+	map_data.boost_pickups = [$boost_pickup, $boost_pickup2]
 	map_data.slingshot_pickups = [$slingshot_pickup]
 	pass
